211a51367e7a333bb08549af98f5c1e35f54e898
Obidoyin Obaloluwa <obaobidoyin@gmail.com>
2023-09-21 17:47:18 +0100

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// todo
+//  add ads
+//  upload it to  github
+//  show ads when you click on those button
+//  add icon to acct
+//  
+//
+//
+const links = document.querySelectorAll('ul.links li a');
+links.forEach(link => {link.setAttribute('target','_blank');
+})
